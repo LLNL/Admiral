b50f59d5fab95ae7261824535e14b9a1036a48e1
Eddie Rusu <rusu1@llnl.gov>
2021-03-10 15:41:43 -0800

    Attemping to add plume state

--- admiral/envs/components/state.py
+++ admiral/envs/components/state.py
@@ -74,6 +74,340 @@ class LifeState:
 
 
 
+# ------------- #
+# --- Plume --- #
+# ------------- #
+
+
+
+class PlumeModel(object):
+    """Simple concentration model with constant Gaussian emission."""
+
+    ORIGIN = np.array([0., 0., 0.])
+    BASE_SYSTEM = np.array([[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]])
+
+    def __init__(self, params=None):
+        """Simple plume model with Gaussian emission. The plume is
+        characterized by the location of the source, the strength of the
+        source, the wind-speed in the x-direction, the disk-area in the
+        y-direction, the disk-area in the z-direction, and the magnitude
+        of noise to add to the samples.
+
+        Parameters:
+            source_coords:        (x, y, z) tuple indicating the location and
+                                  height of the plume
+            strength:             source strength in grams / second
+            wind_speed:           wind speed along x direction in meters/second
+            diffusion_factor_y:   diffusion factor in y direction for urban conditions
+            diffusion_factor_z:   disk area parameter on z direction
+            noise:              variance to add Gaussian noise to observation
+            rng:                random number generator for reproducibility
+        """
+        self.params = params
+        # get range of log values
+        self.params['log_range'] = np.log(self.params['upper_bound']) - np.log(self.params['lower_bound'])
+        # Initilize the discrete coordinate grid used for imaging and inference
+        mesh = {}
+        # prepare grid
+        mesh['x_range'] = np.arange(-1.0, 1.0 + self.params['mesh_distance'], self.params['mesh_distance'])
+        mesh['y_range'] = np.arange(-1.0, 1.0 + self.params['mesh_distance'], self.params['mesh_distance'])
+        mesh['concentrations'] = None
+        self.params['mesh_grid'] = mesh
+
+        # self.params['plume_visual'] = None
+        self.reset(first=True)
+
+    def reset(self, source=[0., 0., 1.], wind_velocity=[1., 0., 0.], first=False):
+        """ Resets the plume model and loads basic parameters depending
+        on wind direction and speed. """
+        # Set wind parameter
+        wind_velocity = np.array(wind_velocity)
+        self.params['wind'] = self.get_wind_params(wind_velocity)
+        self.params['inverse_wind'] = self.get_wind_params(-1. * wind_velocity)
+
+        self.params['source'] = np.array(source)
+        if self.params['source'].shape == (2,):
+            temp = np.array([0., 0., 1.])
+            temp[0:2] = self.params['source'][0:2]
+            self.params['source'] = temp
+        if not first:
+            self.params['mesh_grid'] = self._get_meshgrid()
+            # self.params['plume_visual'] = self._get_plume_visual()
+
+    def get_wind_params(self, wind_velocity):
+        params = {}
+        params['velocity'] = np.array(wind_velocity)
+        params['speed'] = np.linalg.norm(wind_velocity)
+        params['unit_vector'] = self._get_unit_vector(wind_velocity)
+        params['perpendicular_vector'] = self._get_perpendicular_vector(params['unit_vector'])
+        params['angle'] = self._get_angle(params['unit_vector'])
+        params['rotation_matrix'] = self._get_rotation_matrix(
+            self.BASE_SYSTEM[2], -params['angle'])
+        return params
+
+    def get_inverse_concentration_mesh(self, source_coords):
+        '''
+        This function privides concentrations for a mesh grid of discrete
+        coordinates, similar to _get_meshgrid, however it does so for a plume
+        with inverted wind direction. By inputing the sensor_coords for the
+        source_coords of this model we are able to get model predictions of the
+        concentration at the sensor location for hypothetical source locations
+        on the meshgrid. These concentrations can then be used to estimate the
+        likelihood of the source being at that meshgrid location for a sensor
+        concentration measurement at source_coords.
+        TODO: improve paramter notation so it is less confusion, perhaps at the cost of it being less generic.
+        '''
+        return self._get_meshgrid(source=source_coords, wind=self.params['inverse_wind'])
+
+    def get_centered_mesh(self):
+        return self._get_meshgrid(source=np.array((0., 0.)), wind=self.params['wind'])
+
+    def get_concentration(self,
+                          sensor_coords,
+                          source_coords=None,
+                          wind=None):
+        """Generate concentration at given sensor location in micrograms per
+        cubic meter.
+
+        sensor_coords:   Numpy array where each row has the coords of a sensor
+                         and a shape of (x,3)
+        source_coords:   Use instead of actual source_coords when
+                         calculating estimation error, shape (3,)
+        """
+        # print('input sensor_coords')
+        # print(sensor_coords)
+        # print('input source_coords')
+        # print(source_coords)
+
+        if wind is None:
+            wind = self.params['wind']
+        # Reshape sensor coordinates if necessary
+        if sensor_coords.shape == (3,):
+            sensor_coords = sensor_coords[np.newaxis, :]
+
+        # Get source coords if hard-coded
+        if source_coords is None:
+            source_coords = self.params['source']
+        source_coords = self._transform_coords(source_coords, sensor_count=len(sensor_coords))
+        sensor_coords = self._transform_coords(sensor_coords)
+
+        # Get relative coords of sensors to plume source
+        # print('modified sensor_coords')
+        # print(sensor_coords)
+        # print('modified source_coords')
+        # print(source_coords)
+        rel_sensor_coords = sensor_coords - source_coords
+
+        # Wind transformations
+        # print('source_coords')
+        # print(source_coords)
+        # print("wind['perpendicular_vector']")
+        # print(wind['perpendicular_vector'])
+ 
+        # print('source_coords[:, 0:2]')
+        # print(source_coords[:, 0:2])
+        # print("wind['perpendicular_vector'][0:2]")
+        # print(wind['perpendicular_vector'][0:2])
+        line_point = source_coords[:, 0:2] + wind['perpendicular_vector'][0:2]
+        wind_barrier = np.cross(
+            rel_sensor_coords[:, 0:2],
+            line_point - sensor_coords[:, 0:2])
+
+        # Transform sensor coordinates to fit with wind direction
+        if not np.array_equal(wind['unit_vector'], self.BASE_SYSTEM[0]):
+            rel_sensor_coords = self._rotate_coords(
+                wind['rotation_matrix'], rel_sensor_coords, source_coords[0])
+
+        # Calculate concentration using a specific concentration model
+        concentration = self._use_concentration_model(
+            rel_sensor_coords,
+            sensor_coords,
+            source_coords)
+
+        # Add noise if model has noise
+        if self.params['has_noise']:
+            concentration += self.params['rng'].normal(0, np.sqrt(self.params['noise']))
+
+        # clip results for for numerical stability
+        concentration = np.clip(concentration, self.params['lower_bound'], self.params['upper_bound'])
+        # sensors "behind" the plume do not get any signal
+        # Avoid zero values to not mess up log calculations
+        concentration = np.where(
+            wind_barrier > 0,
+            concentration,
+            self.params['offset'])
+
+        # Apply sensitivity factor of sensors
+        concentration *= self.params['sensibility']
+
+        # Bring measurements in range [0,1]
+        if self.params['normalize']:
+            # bring back to sensible values
+            concentration[concentration <= self.params['lower_bound']] = self.params['lower_bound']
+            concentration[concentration > self.params['upper_bound']] = self.params['upper_bound']
+            # take log to have a nicer range
+            concentration = np.log(concentration)
+            # move to positive values
+            concentration -= np.log(self.params['lower_bound'])
+            # Normalize to [0,1] using max log range
+            concentration /= self.params['log_range']
+
+        return concentration
+
+    # def get_visual(self):
+    #     return self.params['plume_visual']
+
+    def gradient(self, sensor_coords, source_coords, observation):
+        """
+            Returns the gradient w.r.t. x_0, y_0, and z_0 for a given location source_coords
+        """
+        return self.grad_lhood(sensor_coords, source_coords, observation)
+
+    def _get_meshgrid(self, source=None, wind=None):
+        if source is None:
+            source = self.params['source']
+        mesh = self.params['mesh_grid']
+        coords = np.array(np.meshgrid(mesh['x_range'], mesh['y_range'], [1.])).T.reshape(-1,3)
+        # Get concentrations
+        concentrations = self.get_concentration(
+            sensor_coords=coords,
+            source_coords=source,
+            wind=wind)
+        # Bring concentrations in right form for plot
+        mesh['concentrations'] = concentrations.reshape(mesh['x_range'].shape[0], mesh['y_range'].shape[0]).T
+        return mesh
+
+    # def _get_plume_visual(self):
+    #     # Plot concentration as mesh
+    #     fig, ax = plt.subplots(frameon=False, figsize=(700, 700), dpi=1)
+    #     fig.subplots_adjust(0,0,1,1)
+    #     cs = ax.contourf(
+    #         self.params['mesh_grid']['x_range'],
+    #         self.params['mesh_grid']['y_range'],
+    #         self.params['mesh_grid']['concentrations'], cmap=matplotlib.cm.OrRd) #cmap='YlOrRd') # levels=10)
+    #     ax.get_xaxis().set_visible(False)
+    #     ax.get_yaxis().set_visible(False)
+    #     fig.canvas.draw()
+    #     buffer = fig.canvas.tostring_rgb()
+    #     buffer_array = np.fromstring(buffer, dtype=np.uint8, sep='')
+    #     visual = buffer_array.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    #     plt.close('all')
+    #     return visual
+
+    def _transform_coords(self, coords, sensor_count=None):
+        """Transform coordinates from 2D to 3D and extend if necessary."""
+        # Properly transform coords into matrix
+        # From [x,y,z] to [[x,y,z],[x,y,z],...,[x,y,z]]
+        if not sensor_count is None:
+            coords = np.repeat(
+                np.expand_dims(coords, axis=0),
+                [sensor_count],
+                axis=0)
+        # Properly transform sensor coords from 2D to 3D if necessary
+        # From [[...,...],[...,...],...] to [[...,...,1.0],[...,...,1.0],...]
+        # From [...,...] to [...,...,1.0]
+        if coords.shape[1] == 2:
+            coords = np.hstack((coords, np.ones((coords.shape[0],1))))
+        return coords
+
+    def _get_unit_vector(self, vector):
+        """ Returns the unit vector of the vector. """
+        return vector / np.linalg.norm(vector)
+
+    def _get_perpendicular_vector(self, vector):
+        """ Returns a vector that is perpenticular to a given vector
+        in the z plane.
+        """
+        return np.cross(self.BASE_SYSTEM[2], vector, axis=0)
+
+    def _get_angle(self, vector, base_vector=None):
+        """ Returns the angle in radians between vectors 'v1' and 'v2'::
+                >>> angle_between((1, 0, 0), (0, 1, 0))
+                1.5707963267948966
+                >>> angle_between((1, 0, 0), (1, 0, 0))
+                0.0
+                >>> angle_between((1, 0, 0), (-1, 0, 0))
+                3.141592653589793
+        """
+        if base_vector is None:
+            base_vector = self.BASE_SYSTEM[0]
+        base_unit_vector = self._get_unit_vector(base_vector)
+        unit_vector = self._get_unit_vector(vector)
+        radian = np.arccos(np.clip(np.dot(base_unit_vector, unit_vector), -1.0, 1.0))
+        if vector[1] >= 0:
+            return radian
+        return -radian
+
+    def _get_rotation_matrix(self, axis, theta):
+        """ Returns the Euler-Rodrigues rotation matrix. """
+        from scipy import linalg
+        return linalg.expm(np.cross(np.eye(3), axis / np.linalg.norm(axis) * theta))
+
+    def _rotate_coords(self, rotation_matrix, coords, source=None):
+        """ Rotate coordinates according to the Euler-Rodrigues rotation matrix. """
+        if source is None:
+            source = self.ORIGIN
+        if coords.shape == (3,):
+            return np.dot(rotation_matrix, coords) #+ source
+        new = np.zeros_like(coords)
+        for i in range(coords.shape[0]):
+            new[i] = self._rotate_coords(rotation_matrix, coords[i]) #+ source
+        return new
+
+    def __str__(self):
+        indent = max([len(key) for key in [*self.params]]) + 5
+        text  = 'PlumeModel_v0:\n'
+        for key, value in self.params.items():
+            text += '    {:_<{}s} {}\n'.format(key, indent, value)
+        return text
+
+    def __repr__(self):
+        return self.__str__()
+
+    def _use_concentration_model(self, rel_sensor_coords, sensor_coords, source_coords):
+        # Calculate dispersion parameters sigma_y and sigma_z
+        # as functions of downwind distance and stability
+        sigma_y = \
+            self.params['diffusion_factor_y'] *\
+            rel_sensor_coords[:,0] *\
+            (1 + 0.0004 * rel_sensor_coords[:,0])**(-0.5)
+        sigma_z = \
+            self.params['diffusion_factor_z'] *\
+            rel_sensor_coords[:,0]
+        # Calculate concentration factors
+        a_t = -0.5 * (rel_sensor_coords[:,2] / sigma_z)**2
+        b_t = -0.5 * ((sensor_coords[:,2] + source_coords[:,2]) / sigma_z)**2
+        c_t = -0.5 * (rel_sensor_coords[:,1] / sigma_y)**2
+        # Put everything together
+        return \
+            (self.params['strength'] / (2 * np.pi * sigma_y * sigma_z * self.params['wind']['speed'])) *\
+            np.exp(c_t) *\
+            (np.exp(a_t) + np.exp(b_t))
+
+    def _use_concentration_model(self, rel_sensor_coords, sensor_coords, source_coords):
+        # Calculate dispersion parameters sigma_y and sigma_z
+        # as functions of downwind distance and stability
+        sigma_y = \
+            self.params['diffusion_factor_y'] *\
+            rel_sensor_coords[:,0] *\
+            (1 + 0.0004 * rel_sensor_coords[:,0])**(-0.5)
+        sigma_z = \
+            self.params['diffusion_factor_z'] *\
+            rel_sensor_coords[:,0]
+        # Calculate concentration factors
+        a_t = -0.5 * (rel_sensor_coords[:,2] / sigma_z)**2
+        b_t = -0.5 * ((sensor_coords[:,2] + source_coords[:,2]) / sigma_z)**2
+        c_t = -0.5 * (rel_sensor_coords[:,1] / sigma_y)**2
+        # Put everything together
+        return \
+            (self.params['strength'] / (2 * np.pi * sigma_y * sigma_z * self.params['wind']['speed'])) *\
+            np.exp(c_t) *\
+            (np.exp(a_t) + np.exp(b_t))
+
+
+
+
+
 # ----------------------------- #
 # --- Position and Movement --- #
 # ----------------------------- #
